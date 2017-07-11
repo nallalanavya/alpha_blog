ffc86c5f4050f6a07a8aa98e1b9f212af4ace66b
navyanallala <navyanallala@gmail.com>
2017-07-11 13:27:55 +0000

    install bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :articles
+  resources :articles
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,6 +9,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'page#home'
   get 'page/about',to:'page#about'
+  resources:articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
